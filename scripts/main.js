2e894dd274c38b578c78b5da07aed33e8285b8d8
ddlpr <103121607+ddlpr@users.noreply.github.com>
2023-08-22 14:58:13 -0700

    Set up single brewery request

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 const main = document.querySelector('#cards-area');
 const CARDS_PER_PAGE = 12;
+const detailsCard = document.querySelector('.details-card');
 
 main.addEventListener('click', (e) => {
   if (e.target.className.includes("btn-details")) {
     console.log(e.target);
-    main.remove();
+    main.style.display = 'none';
+    detailsCard.classList.remove('hidden');
   }
 });
 
@@ -16,12 +18,18 @@ const getBreweries = async () => {
   const data = await response.json();
   return data;
 };
+
+fetch(`https://api.openbrewerydb.org/v1/breweries/${breweryId}`).then((response) => {
+
+})
+
 getBreweries()
 .then(data => {
   for (let i = 0; i < CARDS_PER_PAGE; i++) {
     let element = data[i];
     const breweryCard =
     `<div class="card" style="width: 18rem;">
+      <span style="display: none;">${element.id}</span>
       <div class="card-body">
         <h5 class="card-title">${element.name}</h5>
         <p class="card-text">${element.city}, ${element.state_province}</p>
