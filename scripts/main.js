511c5700864916122d554d34233d8528cdfe0af0
ddlpr <103121607+ddlpr@users.noreply.github.com>
2023-08-22 17:00:03 -0700

    Add dark overlay when popup visible. Close popup when clicking outisde of popup.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,34 @@
 const main = document.querySelector('#cards-area');
 const CARDS_PER_PAGE = 12;
+const popupWrapper = document.querySelector('.popup-wrapper');
 const detailsCard = document.querySelector('.details-card');
+const detailsName = document.querySelector('.details-name');
+const detailsStreetAddress = document.querySelector('.details-streetAddress');
+const detailsCityState = document.querySelector('.details-city-state');
+const detailsPhone = document.querySelector('.details-phone');
+const detailsURL = document.querySelector('.details-url');
+
+popupWrapper.addEventListener('click', () => {
+  popupWrapper.style.display = "none";
+});
 
 main.addEventListener('click', (e) => {
   if (e.target.className.includes("btn-details")) {
-    console.log(e.target);
-    main.style.display = 'none';
-    detailsCard.classList.remove('hidden');
+    let breweryId = e.target.parentElement.parentElement.querySelector('.brewery-id').innerText;
+    getBreweryDetails(breweryId)
+      .then(data => {
+        console.log(data);
+        detailsName.innerText = data.name;
+        detailsStreetAddress.innerText = data.street;
+        detailsCityState.innerText = `${data.city} ${data.state_province}`;
+        detailsPhone.innerText = data.phone;
+        detailsURL.innerText = data.website_url;
+      })
+      .catch(err => {
+        console.log('Error when attempting to fetch brewery.', err);
+      });
+
+    popupWrapper.style.display = 'block';
   }
 });
 
@@ -18,10 +40,15 @@ const getBreweries = async () => {
   const data = await response.json();
   return data;
 };
-
-fetch(`https://api.openbrewerydb.org/v1/breweries/${breweryId}`).then((response) => {
-
-})
+// const breweryId = '5128df48-79fc-4f0f-8b52-d06be54d0cec';
+const getBreweryDetails = async (breweryId) => {
+  const response = await fetch(`https://api.openbrewerydb.org/v1/breweries/${breweryId}`);
+  if (!response.ok) {
+    throw new Error(`Cannot fetch data. ${response.status}`)
+  }
+  const data = await response.json();
+  return data;
+};
 
 getBreweries()
 .then(data => {
@@ -29,7 +56,7 @@ getBreweries()
     let element = data[i];
     const breweryCard =
     `<div class="card" style="width: 18rem;">
-      <span style="display: none;">${element.id}</span>
+      <span class="brewery-id" style="display: none;">${element.id}</span>
       <div class="card-body">
         <h5 class="card-title">${element.name}</h5>
         <p class="card-text">${element.city}, ${element.state_province}</p>
