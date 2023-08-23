3debd7e6f8af7b115cc10ae26672e1e26959eb16
ddlpr <103121607+ddlpr@users.noreply.github.com>
2023-08-23 10:44:01 -0700

    Clean up popup close logic

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,6 +1,7 @@
 const main = document.querySelector('#cards-area');
 const CARDS_PER_PAGE = 12;
 const popupWrapper = document.querySelector('.popup-wrapper');
+const popup = document.querySelector('.popup');
 const detailsCard = document.querySelector('.details-card');
 const detailsName = document.querySelector('.details-name');
 const detailsStreetAddress = document.querySelector('.details-streetAddress');
@@ -8,43 +9,14 @@ const detailsCityState = document.querySelector('.details-city-state');
 const detailsPhone = document.querySelector('.details-phone');
 const detailsURL = document.querySelector('.details-url');
 const pagination = document.querySelector("#pagination");
-let totalPages;
-totalPages = Math.ceil(49 / CARDS_PER_PAGE);
+let currentPage = 1;
 
-updatePagination(1);
+let breweries_arr = [];
 
-function displayTasks (page) {
-  const startIndex = (page - 1) * CARDS_PER_PAGE;
-  const endIndex = startIndex + CARDS_PER_PAGE;
-
-  for (let i = startIndex; i < endIndex && i < tasks.length; i++) {
-    addTask(tasks[i].title, tasks[i].completed);
-  }
-}
-
-function updatePagination(currentPage) {
-  pagination.innerHTML = '';
-
-  for (let i = 1; i <= totalPages; i++) {
-    const pageItem = `<li class='page-item'></li>`;
-    const pageLink = $(`<a class='page-link' href='#'>${i}</a>`)
-
-    if (i === currentPage) {
-      pageItem.addClass("active");
-    }
-
-    pageLink.click(() => {
-      displayTasks(i);
-      updatePagination(i);
-    });
-
-    pageItem.append(pageLink);
-    pagination.append(pageItem);
-  }
-}
-
-popupWrapper.addEventListener('click', () => {
-  popupWrapper.style.display = "none";
+popupWrapper.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target.className.includes('close-details') || !popup.contains(e.target))
+    popupWrapper.style.display = "none";
 });
 
 main.addEventListener('click', (e) => {
@@ -85,23 +57,35 @@ const getBreweryDetails = async (breweryId) => {
   return data;
 };
 
+const buildBreweryCard = (brewery) => {
+  const breweryCard =
+  `<div class="card" style="width: 18rem;">
+    <span class="brewery-id" style="display: none;">${brewery.id}</span>
+    <div class="card-body">
+      <h5 class="card-title">${brewery.name}</h5>
+      <p class="card-text">${brewery.city}, ${brewery.state_province}</p>
+      <a href="#" class="btn btn-primary btn-details">Details</a>
+    </div>
+  </div>`;
+  main.insertAdjacentHTML('afterbegin', breweryCard);
+}
+
+const loadBreweries = () => {
+
+  const startIndex = (currentPage - 1) * CARDS_PER_PAGE;
+  const endIndex = currentPage * CARDS_PER_PAGE;
+  for (let i = startIndex; i < endIndex && i < breweries_arr.length; i++) {
+    let brewery = breweries_arr[i];
+    buildBreweryCard(brewery);
+  }
+
+}
+
 getBreweries()
 .then(data => {
-  for (let i = 0; i < CARDS_PER_PAGE; i++) {
-    let element = data[i];
-    const breweryCard =
-    `<div class="card" style="width: 18rem;">
-      <span class="brewery-id" style="display: none;">${element.id}</span>
-      <div class="card-body">
-        <h5 class="card-title">${element.name}</h5>
-        <p class="card-text">${element.city}, ${element.state_province}</p>
-        <a href="#" class="btn btn-primary btn-details">Details</a>
-      </div>
-    </div>`;
-    main.insertAdjacentHTML('afterbegin', breweryCard);
-
-  }
+  breweries_arr = [...data];
+  loadBreweries();
 })
 .catch(err => {
-  console.log('Rejected:', err.message)
+  console.log('Rejected:', err.message);
 });
